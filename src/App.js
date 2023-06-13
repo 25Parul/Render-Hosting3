7ef41558cde9ceda3f6f7430765391e573004935
paruljain-25 <107891240+paruljain-25@users.noreply.github.com>
2023-06-12 21:31:56 -0700

    Basic render hosting using create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+      The best way to predict the future is to create it.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
