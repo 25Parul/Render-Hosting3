1c3dd3219204b4a7d0770b9adc48a771f7a4d82f
paruljain-25 <107891240+paruljain-25@users.noreply.github.com>
2023-06-13 21:45:23 -0700

    Added / route

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const port = 3020;
 
 app.use(express.static(path.join(__dirname, "/build")))
 
+app.get("/", (req, res)=>{
+    res.send(" The best way to predict the future is to create it.")
+})
+
 app.listen(port, ()=>{
     console.log("Listening on port", port)
 });
